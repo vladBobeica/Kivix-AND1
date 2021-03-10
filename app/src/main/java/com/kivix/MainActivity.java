027b84301c16b60ce5b5bf708c1836ba8cbe1760
vladBobeica <vlad.bobeica@outlook.com>
2021-03-10 13:30:06 +0100

    Getting started

---
+++ app/src/main/java/com/kivix/MainActivity.java
@@ -0,0 +1,41 @@
+package com.kivix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();{
+                    try {
+                        sleep(5000);
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    finally {
+                        Intent welcome = new Intent(MainActivity.this, WelcomeActivity.class);
+                        startActivity(welcome);
+                    }
+                }
+            }
+        };
+        thread.start();
+
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
---
+++ app/src/main/java/com/kivix/WelcomeActivity.java
@@ -0,0 +1,20 @@
+package com.kivix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+
+
+
+
+}
